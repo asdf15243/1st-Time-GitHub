f165641f529475135d1ddc3b65f128c7b2f4fedb
asdf <asdf1790047@gmail.com>
2025-08-30 15:34:30 +0530

    initial commit
    # Conflicts:
    #	README.md

--- README.md
+++
@@ -1,4 +0,0 @@
-# First Time GitHub
-Learning github for first time
-by Mohit Kumar
-hii
---
+++ android/app/src/main/kotlin/com/example/learngithub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.learngithub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
